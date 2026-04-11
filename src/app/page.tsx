3ced55433fe532fcae0d7230d5a6db8358dbcca2
dinda atikah <dindaatikahh@gmail.com>
2026-04-11 12:46:16 +0700

    feat(landing): add landing layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { redirect } from "next/navigation"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/shared/lib/auth"
+import { LandingLayout } from "@/features/landing/components/LandingLayout"
 
-export default async function Home() {
+export default async function LandingPage() {
   const session = await getServerSession(authOptions)
   if (session) redirect("/dashboard")
-  else redirect("/login")
+  return <LandingLayout />
 }
