3548080e2487afe54de32d2f70b850e85b72a55f
dinda atikah <dindaatikahh@gmail.com>
2026-04-11 10:12:38 +0700

    feat(shared): install and configure shadcn/ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import { SessionProvider } from "@/shared/providers/SessionProvider"
+import { Geist } from "next/font/google";
+import { cn } from "@/shared/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "GitPulse",
@@ -12,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="id">
+    <html lang="id" className={cn("font-sans", geist.variable)}>
       <body suppressHydrationWarning>
         <SessionProvider>
           {children}
